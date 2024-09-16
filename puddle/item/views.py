a9739b135f58d39c517067da00e2ce8cbc40b5c9
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-09-16 14:34:03 +0700

    learn Django

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -2,7 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
+
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+    
+    return render(request, 'item/items.html', {
+        'items': items,
+    })
 
 # Create your views here.
 def detail(request, pk):
@@ -34,4 +41,29 @@ def new(request):
         'form': form,
         'title': 'New item',
     })
+    
+@login_required    
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+         
+        if form.is_valid():
+            item.save()
+             
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    
+    return render(request, "item/form.html", {
+        'form': form,
+        'title': 'Edit item',
+    })
+    
+@login_required
+def delete(request,pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+    
+    return redirect('dashboard:index')
 
